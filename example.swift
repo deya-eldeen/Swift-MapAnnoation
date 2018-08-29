36662c380782ac0386ce6f274552f7abce71a5d5
Deya Eldeen Elkhawaldeh <3denator@gmail.com>
2018-08-29 17:05:44 +0300

    Update example.swift

--- example.swift
+++ example.swift
@@ -1,27 +1,66 @@
-var map1 = MKMapView()
-var pointAnnotation:MKPointAnnotation = MKPointAnnotation()
+import UIKit
+import MapKit
+import CoreLocation
 
-func handleTap(gestureReconizer: UITapGestureRecognizer)
-{
-    let location = gestureReconizer.locationInView(map1)
-    let coordinate = map1.convertPoint(location,toCoordinateFromView: map1)
-    pointAnnotation.coordinate = coordinate
+class MapPickerVC: UIViewController {
+
+    var map1 = MKMapView()
+    var pointAnnotation:MKPointAnnotation = MKPointAnnotation()
+
+    @objc func handleTap(gestureReconizer: UITapGestureRecognizer) {
+        let location = gestureReconizer.location(in: map1)
+        let coordinate = map1.convert(location,toCoordinateFrom: map1)
+        pointAnnotation.coordinate = coordinate
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        map1 = self.view.viewWithTag(801) as! MKMapView
+        map1.showsUserLocation = true
+        map1.tintColor = UIColor.blue
+
+        let initialLocation = CLLocation(latitude: Session.lat, longitude: Session.lon )
+        self.centerMapOnLocation(initialLocation, map: map1, regionRadius: 707)
+        var coordinates : CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: Session.lat, longitude: Session.lon)
+        pointAnnotation.coordinate = coordinates
+
+        map1.addAnnotation(pointAnnotation)
+
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action:#selector(handleTap))
+        gestureRecognizer.delegate = self
+        map1.addGestureRecognizer(gestureRecognizer)
+    }
+
+    var locationManager: CLLocationManager!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+    } 
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status != .authorizedWhenInUse {return}
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        let locValue: CLLocationCoordinate2D = manager.location!.coordinate
+        print("locations = \(locValue.latitude) \(locValue.longitude)")
+        Session.lat = locValue.latitude
+        Session.lon = locValue.longitude
+    }
+    
+    func centerMapOnLocation(_ location: CLLocation,map : MKMapView, regionRadius : Double)
+    {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
+        map.setRegion(coordinateRegion, animated: false)
+    }
 }
 
-override func viewDidAppear(animated: Bool)
+extension MapPickerVC : UIGestureRecognizerDelegate {
+
+}
+
+extension MapPickerVC : CLLocationManagerDelegate
 {
-    map1 = self.scrollView.viewWithTag(801) as! MKMapView
-    map1.showsUserLocation = true
-    map1.tintColor = UIColor.blueColor()
 
-    let initialLocation = CLLocation(latitude: ram.currentLat, longitude: ram.currentLon )
-    self.centerMapOnLocation(initialLocation, map: map1, regionRadius: 707)
-    var coordinates : CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: ram.currentLat, longitude: ram.currentLon)
-    pointAnnotation.coordinate = coordinates
-
-    map1.addAnnotation(pointAnnotation)
-
-    let gestureRecognizer = UITapGestureRecognizer(target: self, action:"handleTap:")
-    gestureRecognizer.delegate = self
-    map1.addGestureRecognizer(gestureRecognizer)
 }
